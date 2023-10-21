a0de53c8a39babca7200649a0bce6ddc0eaf0510
danbroz <danbroz@gmail.com>
2023-10-21 01:21:55 -0700

    created console app

---
+++ index.js
@@ -0,0 +1,38 @@
+let API_KEY = "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";
+
+const {OpenAI} = require("openai");
+const readline = require('readline-sync');
+
+const openai = new OpenAI({
+    apiKey: API_KEY
+});
+
+var name = readline.question('Subject to joke about: ');
+
+var frameworks = "These are the 13 Comedy Frameworks: Double Entendres framework is a simple play on words that have cliché takeoffs and reformations. The Reverses framework is a joke structure that infuses the element of surprise through the last-minute switch in assumption. Triple's framework is a 3-way joke structure buildup with tension, manipulation via two logical words or conditions driven towards an expected end, a third exaggeration that shatters this assumption. Incongruity framework is juxtaposing dissimilar elements. Simple Truth framework is a simple play on words that have cliché takeoffs and reformations but with phrases that imply truth in a comedic way. The Superiority framework lures the audience to feel superior through the self-deprecation of the comedian or through attacking people the audience feels inferior to like celebrities. The Paired Phrases framework uses the rhythms of the English lexicon, like antonyms, synonyms, or homonym. Slapstick framework infuses physical gestures into your jokes by acting out. The Observation-Recognition framework exaggerates simple everyday items, events or conditions that are easily recognizable by the audience. The compare and contrast framework operates by inducing surprise by starting out complex but finishing simple. The comedic Irony framework instills surprise with the representation of dramatic irony. The Benign Retaliation framework glamorizes comedic vengeance in a relatable way. The Paradox framework is self-contradictory and allows the audience to think inversely."
+var premise = "You are a funny comedian that doesn't ask questions.  You will be writting jokes about the premise: " + name + ".  You will be using the 13 Comedy Frameworks and don't list the jokes with numbers or Framework type."
+
+
+async function main() {
+
+  let full = "";
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+        {"role": "system", "content": frameworks},
+        {"role": "system", "content": premise},
+        {"role": "user", "content": "write a standup set without asking questions."}],
+    model: 'gpt-4',
+    stream: true,
+  });
+
+  for await (const part of completion) {
+    let text = part.choices[0].delta.content ?? ""
+    full += text;
+
+    console.clear();
+    console.log(full);
+  }
+}
+
+main();
