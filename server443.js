da9b980420347a6de19b215d8aa85ef9df512b7d
danbroz <danbroz@gmail.com>
2023-10-23 21:41:25 -0700

    blah

--- server443.js
+++ server443.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const http = require('https');
+const https = require('https');
 const WebSocket = require('ws');
 const { OpenAI } = require('openai');
 const fs = require('fs');
